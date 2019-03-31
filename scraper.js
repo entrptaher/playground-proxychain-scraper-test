6224637f8924b3dea0a838ebdef8240b2f052c1a
entrptaher@gmail.com <entrptaher@gmail.com>
2019-04-01 05:31:51 +0600

    Add timeouts

--- scraper.js
+++ scraper.js
@@ -3,14 +3,20 @@ const port = process.env.ANYPROXY_PORT || 8001;
 
 async function scraper() {
   const browser = await puppeteer.launch({
+    headless: false,
     args: [`--proxy-server=127.0.0.1:${port}`, "--no-sandbox"]
   });
 
   const page = await browser.newPage();
-  await page.goto("https://httpbin.org/ip");
+  await page.goto("https://httpbin.org/ip", {
+    waitUntil: "networkidle0",
+    timeout: 120000
+  });
   const content = await page.$eval("body", e => e.innerText);
 
   console.log(content);
+
+  await page.screenshot({ path: "screenshots/screenshot.png", fullPage: true });
 
   await browser.close();
 }
